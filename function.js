fb76c37fab8e180556255bbb0c38eaede7e64eb9
vombat11 <gulyas.kata11@gmail.com>
2020-10-26 18:24:11 +0100

    first commit

---
+++ function.js
@@ -0,0 +1,25 @@
+function firstNumber(num1, num2) {
+    console.log(num1 + num2)
+}
+
+firstNumber(5, 17)
+
+firstNumber(11, 23)
+
+function newStudy(name) {
+    return `Hello ${name}`
+}
+
+let greeting = newStudy('Kata')
+
+console.log(greeting)
+
+function modifyGreeting() {
+    greeting = newStudy('Béla')
+}
+
+let x = modifyGreeting()
+
+console.log(greeting, x)
+
+console.log(newStudy('Ferko'))
