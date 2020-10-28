c723cfc8de130d5aa0bc968582d9da38ed3b0985
ferenc.gulyas <ferenc.gulyas@automizy.com>
2020-10-28 21:40:01 +0100

    test cases fixed

--- tests/01_testPlayWithDates.js
+++ tests/01_testPlayWithDates.js
@@ -4,17 +4,20 @@ const calculateMyAge = require("../learning_tasks/playWithDates")
 // here we will test your function
 
 const testYourMagnificantDateFunction = () => {
+  console.log('*** Testing your calculateMyAge function ***')
   const iAmTwenty = calculateMyAge(2000)
   const iAmThirtyFour = calculateMyAge(1986)
   const iAmHundredAndFifteen = calculateMyAge(1905)
 
-  console.log(iAmTwenty) // should be 20
   if (iAmTwenty !== 20) throw new Error("iAmTwenty should be 20")
-  console.log(iAmThirtyFour) // should be 34
+  console.log(`✔️ ${iAmTwenty}`) // should be 20
+
   if (iAmThirtyFour !== 34) throw new Error("iAmThirtyFour should be 34")
-  console.log(iAmHundredAndFifteen) // should be 115
+  console.log(`✔️ ${iAmThirtyFour}`) // should be 34
+
   if (iAmHundredAndFifteen !== 115)
     throw new Error("iAmHundredAndFifteen should be 115")
+  console.log(`✔️ ${iAmHundredAndFifteen}`) // should be 115
 }
 
 try {
