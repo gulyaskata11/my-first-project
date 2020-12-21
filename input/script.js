44c45aaaeda93ea1a4301598e6fce400a7eab973
vombat11 <gulyas.kata11@gmail.com>
2020-12-21 17:57:13 +0100

    inputNew

--- input/script.js
+++ input/script.js
@@ -2,14 +2,23 @@ const input1 = document.getElementById("input1")
 const input2 = document.getElementById("input2")
 const input3 = document.getElementById("input3")
 const button = document.getElementById("button")
-const answer = confirm("Do you want to write the value?")
+const other = document.getElementById("other")
+//const answer = confirm("Do you want to write the value?")
 
 function clickButton() {
-    if (answer) {
-        alert(`${input1.value} (${input2.value}) was born on ${input3.value}`)    
-    } else {
-        alert("Undefined")
+    if (!input1.value) {
+        alert('The name is required!')
+        return
     }
+    if (!input2.value) {
+        alert('The nickname is required!')
+        return
+    }
+    if (!input3.value) {
+        alert('The date is required!')
+        return
+    }
+    other.innerText = `${input1.value} (${input2.value}) was born in ${input3.value}`
 }
 
 button.addEventListener('click', clickButton)
